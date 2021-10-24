ca0791322ba0d57dd03ae3816a5fcde10c07c021
JananiVijayakumar <vsvprema@gmail.com>
2021-10-24 15:34:15 +0530

    Employee Daily Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -8,11 +8,18 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;             //initialize the integer
+        int IS_FULL_TIME = 1;  //initialize the integer
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+        int Daily_Wage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is present");
-        else
+            Daily_Wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Daily Wage of Employee is : " + Daily_Wage);
+        } else {
             System.out.println("Employee is absent");
+            Daily_Wage = Daily_Wage;
+        }
     }
 }
